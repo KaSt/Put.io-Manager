7a89cab905a08aee1d3ca216a1b5184ae28d4c16
K a <k@ka.st>
2014-04-25 22:34:05 +0200

    First test

--- application/models/putio_model.php
+++ application/models/putio_model.php
@@ -118,8 +118,8 @@ class Putio_model extends CI_Model
 
       if (
          file_exists($this->location . 'complete/' . $file['name'])
-         || file_exists($this->location . 'incomplete/' . $file['name']
-         || file_exists($this->tv_path . $file['name']
+         || file_exists($this->location . 'incomplete/' . $file['name'])
+         || file_exists($this->tv_path . $file['name'])
          || file_exists($this->movie_path . $file['name'])
       )
          return true;
